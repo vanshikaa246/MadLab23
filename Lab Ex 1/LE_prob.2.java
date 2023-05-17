6f35fc07969c6fcdf924a835b4baf084e6f9219d
vanshikaa246 <133883081+vanshikaa246@users.noreply.github.com>
2023-05-17 20:30:18 +0530

    Add files via upload

---
+++ Lab Ex 1/LE_prob.2.java
@@ -0,0 +1,26 @@
+public class Main
+{
+  public static void main(String args[])
+  {
+      int n = 4; 
+ 
+    
+
+
+      for(int i=0;i<n;i++)
+      {
+          for(int k=i;k<n;k++)
+          System.out.print(" ");
+ 
+        
+          for(int j=0;j<=i;j++)
+          {
+           
+            System.out.print("* ");
+          }
+ 
+          System.out.println();
+      }
+  }
+}  
+ 
